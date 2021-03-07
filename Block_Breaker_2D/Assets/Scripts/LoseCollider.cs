1f62d77f778a07acff14df35bfc3161513853670
Galin <lemonkiller34@gmail.com>
2021-03-07 22:27:25 +0100

    Added a collider that upon impact shows the game over screen

---
+++ Block_Breaker_2D/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoseCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        // int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // int nextSceneIndex = currentSceneIndex +1;
+        // if(currentSceneIndex >=2)
+        // {
+
+        // }
+
+        SceneManager.LoadScene("Game Over",LoadSceneMode.Single);
+    }
+
+}
